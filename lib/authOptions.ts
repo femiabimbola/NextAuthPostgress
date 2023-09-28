f974d9e16aedfe3f2f934ed0c02974fc9225d5ca
femi <femiabimbola6@gmail.com>
2023-09-28 21:03:42 +0100

    added the auth

---
+++ lib/authOptions.ts
@@ -0,0 +1,54 @@
+import { NextAuthOptions } from 'next-auth';
+import CredentialsProvider from "next-auth/providers/credentials";
+import { db } from "@/lib/prismadb";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { compare } from 'bcrypt';
+
+
+export const authOptions: NextAuthOptions = {
+  pages: {
+    signIn: '/login'
+  },
+  adapter: PrismaAdapter(db),
+  // The session is not compulsory
+  session: {
+    strategy: 'jwt',
+  },
+
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: 'Email', type: 'text', placeholder: '' },
+        password: { label: 'Password', type: "password" },
+        username: { label: "Username", type: 'text', placeholder: 'Enter a username' },
+      },
+      //  The login logic
+      async authorize(credentials) {
+
+        // Check to see if email or password is typed 
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error('missing field')
+        }
+
+        // Check to see if user exists
+        const user = await db.user.findUnique({
+          where: { email: credentials?.email }
+        });
+
+        if (!user || !user?.password) {
+          throw new Error(' No user found')
+        }
+
+        //  Check if the password matches 
+        const passwordMatch = await compare(credentials.password, user.password)
+
+        // if password does not match
+        if (!passwordMatch) throw new Error('incorrect password')
+
+        return { user, id: `${user.id}` };
+      }
+    }),
+  ],
+}
+
